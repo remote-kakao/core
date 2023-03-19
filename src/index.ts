4abc9f3f53da1c9d0aacf6dc36e90e9cceb4a988
thoratica <me@tica.fun>
2023-03-19 22:56:03 +0900

    :art: move example code from src/index.ts to example/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
-import { UDPServer } from './server';
-
-const server = new UDPServer();
-
-server.once('ready', (port) => {
-  console.log(`포트 ${port}에 서버가 열렸어요!`);
-});
-
-server.on('chat', async (msg) => {
-  if (msg.content === '>test') {
-    msg.replyText('안녕하세요!');
-  }
-});
-
-server.start();
+export * from './server';
+export * from './message';
