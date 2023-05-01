cc1a361a20b4ff3b633f37a9391c5539524d1b9b
thoratica <me@tica.fun>
2023-05-01 17:46:38 +0900

    :coffin: Removed unused code

--- example/index.ts
+++ example/index.ts
@@ -1,7 +1,6 @@
 import { UDPServer } from '../src';
 import { MessageLoggerPlugin } from '../src/plugins/MessageLoggerPlugin';
 import path from 'node:path';
-import fs from 'node:fs';
 
 const prefix = '>';
 const server = new UDPServer({ serviceName: 'remote-kakao' });
@@ -19,16 +18,6 @@ server.on('message', async (msg) => {
   const cmd = args.shift()?.slice(prefix.length);
 
   if (cmd === 'ping') {
-    // fs.writeFileSync(
-    //   path.join(process.cwd(), 'profile.png'),
-    //   await msg.sender.profileImage,
-    //   'base64',
-    // );
-    // fs.writeFileSync(
-    //   path.join(process.cwd(), 'icon.png'),
-    //   await msg.room.icon,
-    //   'base64',
-    // );
     /*
       this command's result is the ping between Node.js and MessengerBot,
       not between MessengerBot and the KakaoTalk server.
@@ -36,16 +25,6 @@ server.on('message', async (msg) => {
     const timestamp = Date.now();
     await msg.replyText('Pong!');
     msg.replyText(`${Date.now() - timestamp}ms`);
-  } else if (cmd === 'read') {
-    msg.markAsRead();
-  } else if (cmd === 'containsMention') {
-    msg.replyText(msg.containsMention.toString());
-  } else if (cmd === 'icon') {
-    fs.writeFileSync(
-      path.join(process.cwd(), 'icon.png'),
-      await msg.room.icon,
-      'base64',
-    );
   }
 });
 
